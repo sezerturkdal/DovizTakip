c4904c85c15696258dd466e755d625b8ec111016
Sezer Turkdal <sezer.turkdal@hotmail.com>
2022-11-06 14:31:22 +0300

    Fetching data from realtime database

--- DovizTakip/ViewController.swift
+++ DovizTakip/ViewController.swift
@@ -6,12 +6,27 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        var ref: DatabaseReference!
+        ref = Database.database().reference()
+
+        ref.child("CurrentRates/").getData(completion:  { error, snapshot in
+          guard error == nil else {
+            print(error!.localizedDescription)
+            return;
+          }
+            let value = snapshot?.value as? NSDictionary
+            //let username = value?["username"] as? String ?? ""
+            
+            print(value)
+            //let userName = snapshot?.value as? String ?? "Unknown";
+        });
     }
 
 
