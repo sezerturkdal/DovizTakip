f2fe66965419d7dd49199d643261a6d91c28382d
Sezer Turkdal <sezer.turkdal@hotmail.com>
2022-11-06 22:26:48 +0300

    added spinner

--- DovizTakip/ViewController.swift
+++ DovizTakip/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 import FirebaseDatabase
 
 class ViewController: UIViewController {
+    
+    var spinner = UIActivityIndicatorView(style: .large)
+    var loadingView: UIView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +22,7 @@ class ViewController: UIViewController {
     func getData(){
         var ref: DatabaseReference!
         ref = Database.database().reference()
-
+        showActivityIndicator()
         ref.child("CurrentRates").getData(completion:  { [self] error, snapshot in
           guard error == nil else {
             print(error!.localizedDescription)
@@ -36,6 +39,31 @@ class ViewController: UIViewController {
             }
             
         });
+        hideActivityIndicator()
+    }
+    
+    func showActivityIndicator() {
+            self.loadingView = UIView()
+            self.loadingView.frame = CGRect(x: 0.0, y: 0.0, width: 100.0, height: 100.0)
+            self.loadingView.center = self.view.center
+            self.loadingView.backgroundColor = UIColor.white
+            self.loadingView.alpha = 0.7
+            self.loadingView.clipsToBounds = true
+            self.loadingView.layer.cornerRadius = 10
+
+            self.spinner = UIActivityIndicatorView(style: .large)
+            self.spinner.frame = CGRect(x: 0.0, y: 0.0, width: 80.0, height: 80.0)
+            self.spinner.color = UIColor.blue
+            self.spinner.center = CGPoint(x:self.loadingView.bounds.size.width / 2, y:self.loadingView.bounds.size.height / 2)
+
+            self.loadingView.addSubview(self.spinner)
+            self.view.addSubview(self.loadingView)
+            self.spinner.startAnimating()
+    }
+
+    func hideActivityIndicator() {
+            self.spinner.stopAnimating()
+            self.loadingView.removeFromSuperview()
     }
 
 
